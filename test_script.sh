7e049b1a0a971a3d809293fb0285447e9710e18a
sturo82 <sturo82@gmail.com>
2020-05-27 12:15:50 +0200

    Add files via upload

---
+++ test_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while getopts d:n:db: option
+do
+case "${option}"
+in
+d) DOMAIN=${OPTARG};;#senza il www
+n) APPNAME=${OPTARG};;
+db) DBNAME = ${OPTARG};;
+esac
+done
+
+if [ ! -z "$DBNAME" ]
+  then
+    DBNAME = "$APPNAME";;
+fi
+echo "DBNAME is set to $DBNAME"
+echo "APPNAME is set to $APPNAME"
+echo "DOMAIN is set to $DOMAIN"
